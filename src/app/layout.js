9f7ccdc008aae6fd8c8708179b1e58d7182b549c
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2023-08-29 08:33:57 +0300

    add footer/header/contacts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,25 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-import Header from "@/components/forLayout/Header/Header";
-import Footer from "@/components/forLayout/Footer/Footer";
-import AuthProvider from "@/components/AuthProvider/AuthProvider";
+import './globals.css';
+// import { Inter } from 'next/font/google';
+import { Hahmlet } from 'next/font/google';
+import Header from '@/components/forLayout/Header/Header';
+import Footer from '@/components/forLayout/Footer/Footer';
+import AuthProvider from '@/components/AuthProvider/AuthProvider';
 
-
-const inter = Inter({ subsets: ["latin"] });
+const hahmlet = Hahmlet({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Магазин автомобільних фар AutoOpti",
-  description: "Разом ми зробимо вашу подорож за кермом незабутньою!",
+  title: 'Магазин автомобільних фар AutoOpti',
+  description: 'Разом ми зробимо вашу подорож за кермом незабутньою!',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="uk">
-      <body className={inter.className}>
+      <body className={hahmlet.className}>
         <AuthProvider>
           <Header />
-          {children}
+          <main className="container">{children}</main>
           <Footer />
         </AuthProvider>
       </body>
