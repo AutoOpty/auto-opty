db3c72c957995ee10f32b734afc53ae16cf9da17
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-01-13 14:29:49 +0200

    add products and productsItem

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
-const ProductItem = () => {
-  return <section>ProductItem</section>;
+import styles from './productItem.module.scss';
+
+const ProductItem = ({ photos, title, price, id, code, type }) => {
+  return (
+    <li className={styles.productContainer}>
+      <Link href={`/products/${id}`}>
+        <figure className={styles.imgContainer}>
+          <Image
+            src={photos}
+            alt="apartment"
+            fill
+            className={styles.img}
+            priority
+            sizes="(max-width: 768px) 324px, (max-width: 1440px) 300px"
+          />
+          <figcaption className={styles.codeProduct}>
+            <span className={styles.codeProductItem}>8329533</span>
+          </figcaption>
+        </figure>
+      </Link>
+      <div className={styles.productContent}>
+        <p className={styles.infoProduct}>{title}</p>
+        <p className={styles.infoProduct}>Code:{code}</p>
+        <p className={styles.infoProduct}>TYPE OF HEADLIGHTS : {type}</p>
+        <p className={styles.priceProduct}>{price}$</p>
+      </div>
+      <Link href={`/products/${id}`} className={styles.btnProduct}>
+        Детальніше
+      </Link>
+    </li>
+  );
 };
 
 export default ProductItem;
