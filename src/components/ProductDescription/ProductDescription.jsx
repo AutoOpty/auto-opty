7a1ba9b2058521237b35b61b8dac8bba234502f9
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-01-22 10:31:29 +0200

    add productId tablot/mobile

---
+++ src/components/ProductDescription/ProductDescription.jsx
@@ -0,0 +1,14 @@
+import styles from './ProductDescription.module.scss';
+
+const ProductDescription = () => {
+  return (
+    <p className={styles.description}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, a dolor?
+      Ab, molestiae. Officiis nobis quisquam optio, temporibus similique placeat
+      aliquid adipisci voluptatem quos magnam assumenda, doloremque hic ut
+      saepe.
+    </p>
+  );
+};
+
+export default ProductDescription;
